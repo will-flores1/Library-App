a899c08b01d3b605490ffd8d590ef9c7557a95d9
wilfredo-flores <willflores1221@gmail.com>
2022-09-28 12:37:49 -0400

    add localStrorage api

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,37 @@
 import './scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+import Library from './library'
 
+class Main {
+  // region Properties
+  library;
+  localLibrary;
+  // endregion
+
+  // region Constructor
+  constructor() {
+     this.library = new Library(this)
+     this.localLibrary = this.getLocalStorage();
+  }
+  // endregion
+
+  // region Methods
+  getLocalStorage() {
+    if (!localStorage.getItem("library")) {
+      localStorage.setItem("library", JSON.stringify([]))
+    }
+      return JSON.parse(localStorage.getItem("library")!)
+  }
+
+  saveLocalStorage() {
+    localStorage.setItem("library", JSON.stringify(this.localLibrary))
+  }
+
+  setUp() {
+    
+  }
+  // endregion
+}
+
+const main = new Main
